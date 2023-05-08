921cd69af7d98a07da7c3bc5ce1f8c2e981ff4f3
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2023-05-09 02:40:10 +0900

    Update 없는 숫자 더하기.java

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -3,17 +3,7 @@ class Solution {
         int answer = -1;
         int sum = 0;
         int count = 0;
-        int a = 0;
         
-        // for(int i=0; i<numbers.length; i++) {
-        //     for(int j=0; j<10; j++) {
-        //         if(j == numbers[i])
-        //             count = 1;
-        //         if(count == 0)
-        //             sum += j;
-        //     }
-        //     count = 0;
-        // }
         for(int i=0; i<10; i++) {
             for(int j=0; j<numbers.length; j++)
                 if(i == numbers[j])
