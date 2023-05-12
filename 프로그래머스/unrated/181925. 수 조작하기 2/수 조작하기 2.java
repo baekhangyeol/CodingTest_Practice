886c490d68bb7674cb98c8637ef9250cfa85e8e8
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2023-05-12 15:43:25 +0900

    [unrated] Title: 수 조작하기 2, Time: 10.44 ms, Memory: 90.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i=0; i<numLog.length; i++) {
+            if(i >= 0 && i != numLog.length-1) {
+                if(numLog[i+1] == numLog[i] + 1) {
+                    sb.append("w");
+                }
+                else if(numLog[i+1] == numLog[i] - 1) {
+                    sb.append("s");
+                }
+                else if(numLog[i+1] == numLog[i] + 10) {
+                    sb.append("d");
+                }
+                else if(numLog[i+1] == numLog[i] - 10) {
+                    sb.append("a");
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
