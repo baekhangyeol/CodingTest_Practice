1743c7af777bf737c0fcb718b090d0e4abe67a01
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2023-05-31 16:29:55 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(double num1, double num2) {
+        double answer = 0;
+        double answer1 = num1 / num2;
+        
+        answer = 1000 * (num1 / num2);
+        return (int)answer;
+    }
+}
