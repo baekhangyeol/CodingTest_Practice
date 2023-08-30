80a442f0bbe55de3b4e64cd3a5e9ea04fbc802ab
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2023-08-30 16:42:08 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = new int [(n+1) / 2];
+        int count = 0;
+        
+        for(int i=0; i<=n; i++) {
+            if(i % 2 != 0) {
+                answer[count] = i;
+                count++;
+            }
+            else {
+                continue;
+            }
+        }
+        return answer;
+    }
+}
