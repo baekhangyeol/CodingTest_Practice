e967e890fede90595016a5fa09e9dc4742992382
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2023-09-12 17:51:03 +0900

    [Bronze I] Title: ROT13, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/11655. ROT13/ROT13.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String a = br.readLine();
+
+        for(int i=0; i<a.length(); i++) {
+            char c = a.charAt(i);
+            if(!Character.isLetter(c)) {
+                sb.append(c);
+                continue;
+            }
+            int n = (int)c + 13;
+
+            if(Character.isUpperCase(c) && n > 90) {
+                n -= 26;
+            }
+
+            if(Character.isLowerCase(c) && n > 122) {
+                n -= 26;
+            }
+            sb.append((char)n);
+        }
+        System.out.print(sb);
+    }
+}
