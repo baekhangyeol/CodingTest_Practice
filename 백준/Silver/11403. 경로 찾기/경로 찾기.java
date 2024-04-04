e1ccd5e2896af28dcf0ff8b025f865df89ea91c9
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2024-04-04 18:19:30 +0900

    [Silver I] Title: 경로 찾기, Time: 600 ms, Memory: 30256 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,32 +1,49 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Scanner;
 
 public class Main {
+    static int[][] graph;
+    static boolean[] visited;
+    static int[][] result;
+    static int N;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int[][] graph = new int[N][N];
-        
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+
+        graph = new int[N][N];
+        result = new int[N][N];
+
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
-                graph[i][j] = scanner.nextInt();
+                graph[i][j] = sc.nextInt();
             }
         }
-        
-        for(int k=0; k<N; k++) {
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<N; j++) {
-                    if(graph[i][k] == 1 && graph[k][j] == 1) {
-                        graph[i][j] = 1;
-                    }
-                }
-            }
+
+        for(int i=0; i<N; i++) {
+            visited = new boolean[N];
+            dfs(i ,i);
         }
-        
+
         for(int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
-                System.out.print(graph[i][j] + " ");
+                System.out.print(result[i][j] + " ");
             }
             System.out.println();
+        }
+    }
+
+    static void dfs(int start, int current) {
+        for(int next=0; next<N; next++) {
+            if(!visited[next] && graph[current][next] == 1) {
+                visited[next] = true;
+                result[start][next] = 1;
+                dfs(start, next);
+            }
         }
     }
 }
