2e3108f3d7edaeaf441b0dcf85647a276af69ba2
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2024-05-08 12:57:23 +0900

    [Silver V] Title: 영화감독 숌, Time: 380 ms, Memory: 89320 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        int num = 666;
+        int count = 1;
+
+        while(count != N) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
