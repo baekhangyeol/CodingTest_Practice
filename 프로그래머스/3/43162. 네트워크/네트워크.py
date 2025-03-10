f4dee57470fe80d6889489cdeddddb6ab423c0c2
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2025-03-10 19:48:20 +0900

    [level 3] Title: 네트워크, Time: 1.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,3 @@
 def solution(n, computers):
-    visited = [False] * n
-    
-    def dfs(v):
-        visited[v] = True
-        for i in range(n):
-            if computers[v][i] == 1 and not visited[i]:
-                dfs(i)
-
-    count = 0
-
-    for i in range(n):
-        if not visited[i]: 
-            dfs(i)
-            count += 1
-
-    return count
+    answer = 0
+    return answer
