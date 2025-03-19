af5890eab4af685a138dedbaa63cb071e4930514
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2025-03-19 14:02:24 +0900

    [level 2] Title: 방문 길이, Time: 0.39 ms, Memory: 9.45 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.py
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.py
@@ -1,18 +1,17 @@
 def solution(dirs):
-    x, y = 0, 0
-    visit_route = set()
-    
-    moving = {"U": (0, 1), "D": (0, -1), "R": (1, 0), "L": (-1, 0)}
-    
-    
-    for route in dirs:
-        next_x, next_y = x + moving[route][0], y + moving[route][1]
-        
-        if -5 <= next_x <= 5 and -5 <= next_y <= 5:
-            visit_route.add(((x, y), (next_x, next_y)))
-            visit_route.add(((next_x, next_y), (x, y)))
-            
-            x, y = next_x, next_y
+    visited = set()
+    current = (0, 0)
 
-            
-    return len(visit_route) // 2
+    move = {'U': (0, 1), 'D': (0, -1), 'R': (1, 0), 'L': (-1, 0)}
+
+    for dir in dirs:
+        x = current[0] + move[dir][0]
+        y = current[1] + move[dir][1]
+        
+        if -5 <= x <= 5 and -5 <= y <= 5:
+            visited.add(tuple(sorted([current, (x, y)])))
+            current = (x, y)
+
+    count = len(visited)
+
+    return count
