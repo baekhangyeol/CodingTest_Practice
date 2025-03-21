fa5ac55f247b48688bb7f7e047d37e1e5ee45103
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2025-03-21 15:04:00 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
 
 ### 성능 요약
 
-메모리: 9.14 MB, 시간: 0.06 ms
+메모리: 9.29 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 18일 10:03:49
+2025년 03월 21일 15:03:58
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,19 +1,28 @@
 def solution(name):
     count = 0
-        
-    for a in name:        
-        count += min(ord(a) - ord('A'), ord('Z') - ord(a) + 1)
-        
-    min_move = len(name) - 1
+    upDownmove = 0
+    rightLeftmove = 0
     
-    for i in range(len(name)):
-        next = i+1
-        while next < len(name) and name[next] == 'A':
-            next += 1
-            
-        move = min(i, len(name) - next) * 2 + max(i, len(name) - next)
-        min_move = min(min_move, move)
+    # 조이스틱 위 아래 (최솟값)
+    for spell in name:
+        upDownmove += min(ord(spell) - ord('A'), ord('Z') - ord(spell) + 1)
         
-    count += min_move
-                        
+    # 조이스틱 오 왼
+    # 조이스틱을 오른쪽으로만 계속 움직이는 경우
+    rightLeftmove = len(name) - 1
+    
+    # 조이스틱으로 오른쪽을 가다가 'A'를 만난 경우
+    for i in range(len(name)):
+        # 조이스틱 움직이는 횟수
+        next = i + 1
+        while len(name) > next and name[next] == 'A':
+            next += 1
+        
+        # 오른쪽으로 간 횟수, 오른쪽으로 가다가 A를 만나기 전까지의 움직임 횟수 중 최소의 2배 (앞으로 갔다가 뒤로 이동)
+        # 오른쪽으로 간 횟수, 오른쪽으로 가다가 A를 만나기 전까지의 움직임 횟수 중 최대
+        move = min(i, len(name) - next) * 2 + max(i, len(name) - next)
+        rightLeftmove = min(rightLeftmove, move)
+            
+    count = upDownmove + rightLeftmove
+    
     return count
