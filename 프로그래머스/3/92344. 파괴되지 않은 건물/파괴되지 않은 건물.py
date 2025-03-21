b405e8f329bdb2bb554f9eb762024e7b0f09b307
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2025-03-21 15:16:28 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 916.43 ms, Memory: 143 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,29 +1,37 @@
 def solution(board, skill):
     count = 0
-
-    diff = [[0] * (len(board[0])+1) for _ in range(len(board) + 1)]
-
+        
+    prefix = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)]
+        
     for type, r1, c1, r2, c2, degree in skill:
+        
         if type == 1:
             degree = -degree
 
-        diff[r1][c1] += degree
-        diff[r1][c2+1] -= degree
-        diff[r2+1][c1] -= degree
-        diff[r2+1][c2+1] += degree
-
+        prefix[r1][c1] += degree
+        prefix[r1][c2 + 1] -= degree
+        prefix[r2 + 1][c1] -= degree
+        prefix[r2 + 1][c2 + 1] += degree
+        
+        
+    # 세로 누적합
     for i in range(len(board)):
         for j in range(len(board[0])):
-            diff[i][j+1] += diff[i][j]
-
+            prefix[i][j+1] += prefix[i][j]
+            
+            
+    # 가로 누적합
     for i in range(len(board)):
         for j in range(len(board[0])):
-            diff[i+1][j] += diff[i][j]
-
+            prefix[i+1][j] += prefix[i][j]
+            
+            
+    # prefix를 board에 반영
     for i in range(len(board)):
         for j in range(len(board[0])):
-            board[i][j] += diff[i][j]
+            board[i][j] += prefix[i][j]
+            
             if board[i][j] > 0:
                 count +=1
-
+        
     return count
