493c032eaf9a368b4474fd1d830307ac383cdd56
HanGyeol Baek <76465887+baekhangyeol@users.noreply.github.com>
2025-04-16 15:10:52 +0900

    [Gold V] Title: 합분해, Time: 40 ms, Memory: 34456 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -0,0 +1,17 @@
+def main():
+    N, K = map(int, input().split())
+    dp = [[0] * (N + 1) for _ in range(K + 1)]
+
+    for i in range(K + 1):
+        dp[i][0] = 1
+
+    for i in range(1, K + 1):
+        for j in range(1, N + 1):
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000000
+
+    print(dp[K][N])
+
+
+
+if __name__ == '__main__':
+    main()
